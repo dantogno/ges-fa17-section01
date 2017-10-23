ee68105255d92565c5a1a5c47a8e7f4d06f87f33
David Antognoli <vebulidbih@msn.com>
2017-10-23 16:59:25 -0500

    coin town

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    static int coinCount = 0;
+
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+
+    private void Start()
+    {
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            audioSource.Play();
+            coinCount++;
+            Debug.Log("Touched the coin. Coincount: " + coinCount);
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+            //Destroy(gameObject);
+        }
+    }
+}
